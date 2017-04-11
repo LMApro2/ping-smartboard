e48109cb41bae0949948c76f781d901ab0f648ca
Minh Anh Lam <anh.lamminh@vndirect.com.vn>
2017-04-11 16:52:39 +0700

    Init project: determine the nearest domain and navigate to it

---
+++ main.js
@@ -0,0 +1,23 @@
+var SMARTBOARD_URLS = [
+	'https://banggia-hn.vndirect.com.vn',
+	'https://banggia-hcm.vndirect.com.vn',
+	'https://banggia-na.vndirect.com.vn',
+	'https://banggia-eu.vndirect.com.vn',
+	'https://banggia-as.vndirect.com.vn'
+];
+
+function shuffle(a) {
+    for (var i = a.length; i; i--) {
+        var j = Math.floor(Math.random() * i);
+        var tmp = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = tmp;
+    }
+}
+
+for (var i in SMARTBOARD_URLS) {
+	ping(SMARTBOARD_URLS[i]).then(function(data) {
+		window.location.replace(data.url);
+	});	
+}
+
